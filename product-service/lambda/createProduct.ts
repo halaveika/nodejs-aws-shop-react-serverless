a1144d237669e5bff026c7f31e7a0607b2bf083c
Aliaksandr <halaveika1988@gmail.com>
2023-06-13 08:16:21 +0200

    add transsaction logic, fix createproduct method, update swagger

--- product-service/lambda/createProduct.ts
+++ product-service/lambda/createProduct.ts
@@ -1,9 +1,11 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { Client } from 'pg';
+import { v4 as uuidv4 } from 'uuid';
 
 export async function createProduct(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
   try {
     console.log('Incoming createProduct request:', event);
+
     const client = new Client({
       user: process.env.PG_DB_USER || '',
       host: process.env.PG_DB_HOST || '',
@@ -13,37 +15,55 @@ export async function createProduct(event: APIGatewayProxyEvent): Promise<APIGat
     });
     await client.connect();
 
+    // Start the transaction
+    await client.query('BEGIN');
+
     const { title, description, price, count } = JSON.parse(event.body || '');
 
     if (!title || !description || !price || !count) {
+      await client.query('ROLLBACK');
       return {
         statusCode: 400,
         body: JSON.stringify({ message: 'Invalid product data' })
       };
     }
 
-    const query = `
-      INSERT INTO products (title, description, price)
-      VALUES ($1, $2, $3)
-      RETURNING id
-    `;
-    const result = await client.query(query, [title, description, price]);
-    const productId = result.rows[0].id;
+    const productId = uuidv4();
 
-    const stockQuery = `
-      INSERT INTO stocks (product_id, count)
-      VALUES ($1, $2)
-    `;
-    await client.query(stockQuery, [productId, count]);
+    try {
+      const productQuery = `
+        INSERT INTO products (id, title, description, price)
+        VALUES ($1, $2, $3, $4)
+      `;
+      await client.query(productQuery, [productId, title, description, price]);
 
-    await client.end();
+      const stockQuery = `
+        INSERT INTO stocks (product_id, count)
+        VALUES ((SELECT id FROM products WHERE id = $1), $2)
+      `;
+      await client.query(stockQuery, [productId, count]);
 
-    return {
-      statusCode: 201,
-      body: JSON.stringify({ message: 'Product created successfully' })
-    };
+      // Commit the transaction
+      await client.query('COMMIT');
+
+      return {
+        statusCode: 201,
+        body: JSON.stringify({ message: 'Product created successfully' })
+      };
+    } catch (error) {
+      // Rollback the transaction
+      await client.query('ROLLBACK');
+
+      console.error('Error creating product:', error);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ message: 'Internal Server Error' })
+      };
+    } finally {
+      await client.end();
+    }
   } catch (error) {
-    console.error('Error creating product:', error);
+    console.error('Error connecting to the database:', error);
     return {
       statusCode: 500,
       body: JSON.stringify({ message: 'Internal Server Error' })
