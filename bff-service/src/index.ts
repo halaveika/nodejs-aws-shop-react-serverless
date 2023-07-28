ec9b68eea143f9abbfe929fb25d7a91776ff04cd
Aliaksandr <halaveika1988@gmail.com>
2023-07-28 10:19:27 +0200

    feat: add elasticbeanstalk infrostructure

--- bff-service/src/index.ts
+++ bff-service/src/index.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response, NextFunction } from 'express';
 import axios, { AxiosResponse, Method } from 'axios';
 import dotenv from 'dotenv';
-import { URLSearchParams } from 'url';
 
 dotenv.config();
 
@@ -13,7 +12,7 @@ app.use(express.json());
 app.use(async (req: Request, res: Response, next: NextFunction) => {
   const recipientServiceName = req.params.recipientServiceName;
   const recipientURL = process.env[recipientServiceName];
-  const query = new URLSearchParams(req.query).toString();
+  const query = req.query;
 
   if (!recipientURL) {
     return res.status(502).json({ error: 'Cannot process request' });
@@ -21,14 +20,16 @@ app.use(async (req: Request, res: Response, next: NextFunction) => {
 
   try {
     const { method, body } = req;
+    const queryString = Object.keys(query).length > 0 ? `?${require('querystring').stringify(query)}` : '';
+
     const response: AxiosResponse = await axios({
       method: method as Method,
-      url: query ? `${recipientURL}?${query}` : recipientURL,
+      url: `${recipientURL}${queryString}`,
       data: body,
     });
 
     return res.status(response.status).json(response.data);
-  } catch (error) {
+  } catch (error: any) { 
     return res.status(error.response?.status || 500).json(error.response?.data || { error: 'Internal Server Error' });
   }
 });
